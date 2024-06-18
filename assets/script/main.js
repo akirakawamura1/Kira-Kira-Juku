593877e12f2ffd260537af4f0a5656b8413643d7
akirakawamura1 <nagomi12002@gmail.com>
2024-06-18 23:23:45 +0900

    2回目修正sp完成

---
+++ assets/script/main.js
@@ -0,0 +1,25 @@
+var headerMenuButton = document.querySelector('.header_menu_button');
+var headerNav = document.querySelector('.header_nav');
+
+headerMenuButton.addEventListener('click', function() {
+    if (headerNav.style.display === 'none' || headerNav.style.display === '') {
+        headerNav.style.display = 'flex';
+    } else {
+        headerNav.style.display = 'none';
+    }
+});
+
+function adjustMenuDisplay() {
+    var width = window.innerWidth;
+
+    if (width >= 768) {
+        headerMenuButton.style.display = 'none';
+        headerNav.style.display = 'block';
+    } else {
+        headerMenuButton.style.display = 'flex';
+        headerNav.style.display = 'none';
+    }
+}
+
+window.addEventListener('resize', adjustMenuDisplay);
+window.addEventListener('DOMContentLoaded', adjustMenuDisplay);
